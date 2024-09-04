6e570109bbd5c919948606e2652b1db6da1e9b2a
Venisha Kalola <venishakalola@gmail.com>
2024-09-04 22:02:25 +0530

    routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,13 +8,16 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-logo">BONVOYAGE</div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/home">Home</Link></li>
+ 
         <li><Link to="/hotel">Hotel</Link></li>
         <li><Link to="/guide">Guide</Link></li>
         <li><Link to="/connect">Discover</Link></li>
         <li><Link to="/itinerary">Itinerary</Link></li>
       </ul>
-      <button className="login-btn">Login</button>
+      <Link to="/">
+        <button className="login-btn">Login</button>
+      </Link>
     </nav>
   );
 };
