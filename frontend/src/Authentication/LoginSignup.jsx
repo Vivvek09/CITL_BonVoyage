a4609a47c11f7b3fdb95380263eeb854fa091ccb
Rashii1218 <146517140+Rashii1218@users.noreply.github.com>
2024-11-05 02:42:00 +0530

    Update LoginSignup.jsx

--- frontend/src/Authentication/LoginSignup.jsx
+++ frontend/src/Authentication/LoginSignup.jsx
@@ -22,12 +22,7 @@ const LoginSignup = () => {
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      // const signupResponse = await axios.post("http://localhost:3000/api/signup", {
-      //   name,
-      //   email,
-      //   password,
-      // });
-      const signupResponse = await axios.post("https://citl-bonvoyage-backend-1td7.onrender.com/api/signup", {
+      const signupResponse = await axios.post("http://localhost:3000/api/signup", {
         name,
         email,
         password,
@@ -35,7 +30,7 @@ const LoginSignup = () => {
       console.log(signupResponse.data);
 
       // Automatically log in the user after successful signup
-      const loginResponse = await axios.post("https://citl-bonvoyage-backend-1td7.onrender.com/api/login", {
+      const loginResponse = await axios.post("http://localhost:3000/api/login", {
         email,
         password,
       }, { withCredentials: true });
@@ -52,7 +47,7 @@ const LoginSignup = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://citl-bonvoyage-backend-1td7.onrender.com/api/login", {
+      const response = await axios.post("http://localhost:3000/api/login", {
         email,
         password,
       }, { withCredentials: true });
